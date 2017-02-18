e6227c21fe1fc9b94caa5d90e021c2139d61c781
Nikhil Agrawal <nikhil.agrwl07@gmail.com>
2017-02-18 16:21:27 +0530

    adding java8 (lambda + stream)

---
+++ src/Java8/Employee.java
@@ -0,0 +1,40 @@
+package Java8;
+
+/**
+ * Created by nikhilagrawal on 18/02/17.
+ */
+public class Employee {
+
+    String name;
+    int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
