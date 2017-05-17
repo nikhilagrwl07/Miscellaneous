3e5c6ef66b22081cfcff2bb36d2cdbae1bfe28ed
Nikhil.Agrawal <nikhil.agrawal@target.com>
2017-05-17 16:23:15 +0530

    Single Abstract Method (SAM) design pattern

--- src/Java8Constructs/LambdaAndStreams/Java8FunctionExamples.java
+++ src/Java8Constructs/LambdaAndStreams/Java8FunctionExamples.java
@@ -1,6 +1,7 @@
 package Java8Constructs.LambdaAndStreams;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 /**
@@ -13,7 +14,6 @@ public class Java8FunctionExamples {
 
 
     Function<String, Integer> lengthofUserName = (s) -> s.length();
-    //    System.out.println(lengthofUserName.apply("Nikhil"));
 
     Function<Integer, Boolean> isLengthNameGreaterThan4 = (x) -> x > 4;
 
@@ -30,6 +30,21 @@ public class Java8FunctionExamples {
     Function<Integer, Boolean> isDivisble = (number) -> IntStream.range(2, number).noneMatch(divisor -> number % divisor == 0);
     System.out.println("IsPrime Function - " + isDivisble.apply(30));
 
+    //Question -  Find double of first even number which is greater than 3. Please note that
+    // there we can use stream , then filter and then map and then findFirst() as another way
+    // to find double of first even number which is greater than 3
+    Predicate<Integer> isDivisblePredicate = (x) -> (x > 3 && x % 2 == 0);
+    Function<Integer, Integer> firstNumber = (x) -> IntStream.range(1, x).filter(n -> isDivisblePredicate.test(n)).findFirst().getAsInt();
+    Function<Integer, Integer> doubleNumber = (x) -> x * 2;
+    Function<Integer, Integer> result = x -> firstNumber.andThen(doubleNumber).apply(x);
+    System.out.println("Double of first even number which is greater than 3  is : " + result.apply(10));
+
+
+    // Sample code using both Function and Predicate. Question is finding whether a number is greater than given number
+    Function<Integer, Predicate<Integer>> isGreaterThanXFunction = (pivot) -> (number -> number > pivot);
+    Predicate<Integer> isGreaterThan10 = isGreaterThanXFunction.apply(10);
+    Predicate<Integer> isGreaterThan20 = isGreaterThanXFunction.apply(20);
+    Predicate<Integer> isGreaterThan100 = isGreaterThanXFunction.apply(100);
   }
 
 
