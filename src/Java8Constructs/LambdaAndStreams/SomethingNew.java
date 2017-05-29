eb30d8f9c8c6c948178a467c9a9e55fbab3df2f9
Nikhil.Agrawal <nikhil.agrawal@target.com>
2017-05-29 21:37:25 +0530

    changes

--- src/Java8Constructs/LambdaAndStreams/SomethingNew.java
+++ src/Java8Constructs/LambdaAndStreams/SomethingNew.java
@@ -1,6 +1,10 @@
 package Java8Constructs.LambdaAndStreams;
 
+import java.io.Serializable;
+import java.util.*;
+import java.util.function.IntUnaryOperator;
 import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
 
 /**
  * @author Nikhil Agrawal on 5/20/17.
@@ -14,6 +18,30 @@ public class SomethingNew {
 
     ToIntFunction<String> fun = String::length;
     System.out.println(fun.applyAsInt("Nikhil"));
+
+    demoLambda integerdemoLambda = (x, y) -> (x+y);
+    System.out.println(integerdemoLambda.applyOperation(10,20));
+
+    IntUnaryOperator addOneShort= x -> x+1;
+    IntUnaryOperator addOneLong= x -> {return x+1;};
+
+
+
+    Map<String, Integer> map = new HashMap<>();   // ... or any other Map class
+    map.put("nikhil", 5);
+    map.put("cccccc", 4);
+    map.put("aaaaaa",3);
+    map.put("zzzzzz", 3);
+    map.put("bbbbbbb", 2);
+
+    map = map.entrySet()
+        .stream()
+        .sorted(Map.Entry.comparingByValue())
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k, v) -> k, LinkedHashMap::new));
+
+
+    TreeSet<Long> ts = new TreeSet<>((Comparator<Long> & Serializable) (x, y) -> Long.compare(y, x));
+
   }
 
 }
@@ -22,4 +50,10 @@ public class SomethingNew {
 @FunctionalInterface
  interface binOps{
   int apply(int value1, int value2);
+}
+
+
+@FunctionalInterface
+interface demoLambda{
+  public int applyOperation(int x , int y);
 }
